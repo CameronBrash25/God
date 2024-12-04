8289af22d45c68b524bc11898ef41d8536e822e8
CameronBrash25 <cbrash2501@gmail.com>
2024-12-04 14:55:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function addTypingAnimation(prefix, message) {
         textArea.scrollTop = textArea.scrollHeight; // Keep chat scrolled to bottom
     }
 
-    const typingInterval = setInterval(typeChar, 130); // Slower speed (100ms per character)
+    const typingInterval = setInterval(typeChar, 105); // Slower speed (100ms per character)
 }
 
 
