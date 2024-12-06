5e18018d9de70b9c56068195158199f2412e14c1
CameronBrash25 <cbrash2501@gmail.com>
2024-12-05 23:57:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -232,7 +232,7 @@ function playGongSound() {
 // Fetch a placeholder response from the backend
 async function getGodResponse(question) {
     try {
-        const response = await fetch("https://god-hjjh.onrender.com", {
+        const response = await fetch("https://god-hjjh.onrender.com/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
